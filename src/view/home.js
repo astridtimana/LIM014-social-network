30f9d3ac323b74c6c9bdfff79b03d0dd7068c25e
Astrid Timana <astrid.1295@gmail.com>
2021-05-06 18:54:02 -0500

    maquetando con CSS

--- src/view/home.js
+++ src/view/home.js
@@ -1,27 +1,28 @@
 export default () => {
   const viewHome = `
     <header>
-      <h1> PRIDE </h1>
-      <img class="" src="" alt="">
+      <article id="logoContainer">
+        <img class="logo" src="./images/colorwheel.png"><h1> PRIDE </h1>
+      </article>
     </header>
-    <section class= "home">
+    <section>
       <input type="email" placeholder="Correo" id="correo"> 
       <input type="password" placeholder="Contraseña" id="contraseña"> 
-      <input type="checkbox" id="cbox2" value="second_checkbox" class=""> <label for="cbox2">Recuérdame</label>
-      <a>Olvidé mi contraseña</a>
-      <button>Ingresar</button>
+      <article class="smallContainer">
+        <article id="recuérdame"><input type="checkbox"><label class="smallText">Recuérdame</label></article>
+        <a class = "smallText">Olvidé mi contraseña</a>
+      </article>
+      <a class= "ingresar">Ingresar</a>
       <i class="fas fa-arrow-right"></i>
-      <button>Ingresar con Google</button>
+      <button class = "googleButton">Ingresar con Google</button>
+      <article class="smallContainer">
       <p>¿No tienes una cuenta?</p>
       <a href="#/register">Regístrate</a>
-
-      <button>Olvidé mi contraseña</button>
+      </article>
     </section>
-    <figure class="">
-        <img class="" src="" alt="">
-    </figure>
     `;
   const sectionElement = document.createElement('section');
+  sectionElement.setAttribute('class', 'home');
   sectionElement.innerHTML = viewHome;
   return sectionElement;
 };
