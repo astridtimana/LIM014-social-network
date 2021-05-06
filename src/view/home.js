de5ad3ee11f92a544111ed56b37b377e3f1cbe81
VillalobosNicole <nicolevillalobosbe@gmail.com>
2021-05-06 17:56:01 -0500

    añadiendo estilo al CSS

--- src/view/home.js
+++ src/view/home.js
@@ -4,10 +4,13 @@ export default () => {
       <h1> PRIDE </h1>
       <img class="" src="" alt="">
     </header>
-    <section>
+    <section class= "home">
       <input type="email" placeholder="Correo" id="correo"> 
       <input type="password" placeholder="Contraseña" id="contraseña"> 
+      <input type="checkbox" id="cbox2" value="second_checkbox" class=""> <label for="cbox2">Recuérdame</label>
+      <a>Olvidé mi contraseña</a>
       <button>Ingresar</button>
+      <i class="fas fa-arrow-right"></i>
       <button>Ingresar con Google</button>
       <p>¿No tienes una cuenta?</p>
       <a href="#/register">Regístrate</a>
@@ -18,7 +21,7 @@ export default () => {
         <img class="" src="" alt="">
     </figure>
     `;
-  const divElement = document.createElement('div');
-  divElement.innerHTML = viewHome;
-  return divElement;
+  const sectionElement = document.createElement('section');
+  sectionElement.innerHTML = viewHome;
+  return sectionElement;
 };
