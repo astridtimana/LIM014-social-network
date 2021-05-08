b620e18a3bf9a56acd181e19dfba556df913ec14
Astrid Timana <astrid.1295@gmail.com>
2021-05-07 20:25:49 -0500

    testeando función home

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,13 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+// import { myFunction } from '../src/lib/index';
+// import { template } from '@babel/core';
+import Home from '../src/view/home';
 
-describe('myFunction', () => {
+describe('Función home', () => {
   it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+    expect(typeof Home).toBe('function');
+  });
+  it('debería retornar Html strings ', () => {
+    expect(Home()).toEqual(expect.any(Object));
   });
 });
