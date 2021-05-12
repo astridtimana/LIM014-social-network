5bc391aaaca770a28585c55dc7712d52c792348d
VillalobosNicole <nicolevillalobosbe@gmail.com>
2021-05-12 01:43:37 -0500

    agregando icon a title

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-console */
 // Este es el punto de entrada de tu aplicacion
@@ -5,7 +6,6 @@
 import { myFunction } from './lib/index.js';
 
 myFunction(); */
-
 
 import { changeView } from './view-controller/indexMVC.js';
 
@@ -18,9 +18,16 @@ const init = () => {
 };
 
 window.addEventListener('load', init);
-
+/*
 function signUpUser() {
   console.log('wiw');
 }
 
-document.querySelector('#arrowImgRegister').addEventListener('click', signUpUser);
+document.querySelector('#arrowImgRegister').addEventListener('click', signUpUser); */
+
+const prueba = document.getElementById('arrowImgRegister');
+prueba.addEventListener('click', () => {
+  const email = document.getElementById('correo2').value;
+  const pass = document.getElementById('password').value;
+  alert(`email=${email}pass= ${pass}`);
+});
