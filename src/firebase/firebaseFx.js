1fa46fc00d5cd0985e2f4bda1d9ad6e191372427
Astrid Timana <astrid.1295@gmail.com>
2021-05-17 10:25:50 -0500

    vista feed

--- src/firebase/firebaseFx.js
+++ src/firebase/firebaseFx.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-multiple-empty-lines */
 /* eslint-disable no-alert */
 /* eslint-disable max-len */
 /* eslint-disable no-use-before-define */
@@ -25,3 +26,76 @@ const verificationMail = () => {
     console.log(`Error: ${error}`);
   });
 };
+
+
+
+// Configura la contraseña de un usuario
+// Para configurar la contraseña de un usuario, puedes usar el método updatePassword. Por ejemplo:
+
+// const user = firebase.auth().currentUser;
+// const newPassword = getASecureRandomPassword();
+
+// user.updatePassword(newPassword).then(() => {
+//   // Update successful.
+// }).catch((error) => {
+//   // An error happened.
+// });
+
+
+
+// Envía un correo electrónico de restablecimiento de contraseña
+// Para enviar un correo electrónico de restablecimiento de contraseña a un usuario, puedes usar el método sendPasswordResetEmail. Por ejemplo:
+
+// const auth = firebase.auth();
+// const emailAddress = 'user@example.com';
+
+// auth.sendPasswordResetEmail(emailAddress).then(() => {
+//   // Email sent.
+// }).catch((error) => {
+//   // An error happened.
+// });
+
+
+
+
+// Cuando un usuario accede a tu app, pasa la dirección de correo electrónico y la contraseña a signInWithEmailAndPassword:
+// Web v8
+// Web v9
+
+// firebase.auth().signInWithEmailAndPassword(email, password)
+//   .then((userCredential) => {
+//     // Signed in
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//   });
+
+
+
+
+// Para salir de la sesión de un usuario, llama a signOut de la siguiente manera:
+
+// Web v8
+// Web v9
+
+// firebase.auth().signOut().then(() => {
+//   // Sign-out successful.
+// }).catch((error) => {
+//   // An error happened.
+// });
+
+
+
+// Obtén el usuario con sesión activa
+// La manera recomendada de obtener el usuario actual es establecer un observador en el objeto Auth:
+
+// firebase.auth().onAuthStateChanged(function(user) {
+//   if (user) {
+//     // User is signed in.
+//   } else {
+//     // No user is signed in.
+//   }
+// });
--- src/view/feed.js
+++ src/view/feed.js
@@ -7,16 +7,42 @@ export default () => {
           <img id="searchIcon" src="./images/searchIcon.png">
           <input id="searchBar" type="text" placeholder="Encuentra a tus amigos..." name="search">
         </section>
-        <p> Profile </p>
+        <p id="profile"> Profile </p>
+        <label for="menu-toggle">
+          <div class="botonMenu">
+            <img id="hamburguerBtn" src="./images/haburguerMenu.png">
+          </div>
+       </label>
+       <input type="checkbox" id="menu-toggle"/>
+        <section id="trickMenu">
+          <ul >
+            <li><a href="#"> Configuración </a></li>
+            <li><a href="#"> Adicionales </a></li>
+          </ul>
+        </section>
       </nav>
+
     </header>
       
     <section id="feedConfigArea">
       <section class="feedConfig">
-        <img class="configIcon" id="favoriteIcon" src="./images/favorite.png">
-        <img class="configIcon" id="profileIcon" src="./images/profile.png">
-        <img class="configIcon" id="settingsIcon" src="./images/settings.png">
-        <img class="configIcon" id="logOutIcon" src="./images/logOut.png">
+        <section class="feedConfigOption">
+          <img class="configIcon" id="favoriteIcon" src="./images/favorite.png"> 
+          <p>Inicio</p>
+        </section>
+        <section class="feedConfigOption">
+          <img class="configIcon" id="profileIcon" src="./images/profile.png"> 
+          <p> Mi perfil</p>
+        </section>
+        <section class="feedConfigOption">
+          <img class="configIcon" id="settingsIcon" src="./images/settings.png"> 
+          <p>Configuración</p>
+        </section>
+        <section class="feedConfigOption">
+          <img class="configIcon" id="logOutIcon" src="./images/logout.png"> 
+          <p>Salir</p>
+        </section>
+
       </section>
     </section>
 
