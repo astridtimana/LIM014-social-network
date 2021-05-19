2657af9bd4cb2f115680014490e1ac729c611b8b
VillalobosNicole <nicolevillalobosbe@gmail.com>
2021-05-18 20:13:19 -0500

    empezando test de catch (inconcluso)

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -26,6 +26,17 @@ const mocksdk = new firebasemock.MockFirebaseSdk(
 
 global.firebase = mocksdk;
 
+const objReject = {
+email: "fake",
+password: " ",
+};
+
+const correctObject = {
+email: "ben@example",
+password: "examplePass",
+}
+
+
 describe('Función que crea un nuevo usuario sin tener cuenta de Google', () => {
   it('Debería ser una función', () => {
     expect(typeof createUser).toBe('function');
@@ -34,6 +45,11 @@ describe('Función que crea un nuevo usuario sin tener cuenta de Google', () => 
     .then((user) => {
       expect(user.email).toBe('ben@example.com');
     }));
+  test('mock promise rejection', async () => {
+        mockauth.mockRejectedValue(objReject)
+        return (createUser(correctObject)).then((user) => {
+          expect(user.email).toEqual(objRejectionB.email)
+        })
 });
 
 describe('Función que permite verificar el correo', () => {
