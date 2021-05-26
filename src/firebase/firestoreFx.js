aa79ddd2ed42a422c3e34cdb6b959c4c4f6e8066
Yessenia <vela.kathy@gmail.com>
2021-05-26 16:19:02 -0500

    agrega un comentario

--- src/firebase/firestoreFx.js
+++ src/firebase/firestoreFx.js
@@ -1,4 +1,4 @@
-const firestore = firebase.firestore();
+const firestore = firebase.firestore(); // desde firebase, voy a llamar algo llamado firestore
 
 export const newPost = (doc) => {
   firestore.collection('posts').add(doc);
