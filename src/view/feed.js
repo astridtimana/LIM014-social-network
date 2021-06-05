d9705f18075e7fae41cd8df21846953983ed0107
Yessenia <vela.kathy@gmail.com>
2021-06-05 00:25:18 -0500

    no duplica las imágenes cargadas luego de presionar el botón publicar

--- src/view/feed.js
+++ src/view/feed.js
@@ -10,7 +10,7 @@ import {
 } from '../firebase/firebaseFx.js';
 import templatePost from './posts.js';
 // console.log(templatePost());
-import { addDocPost, listPostAll, onGetPosts } from '../firebase/firestoreFx.js';
+import { addDocPost, listPostAll } from '../firebase/firestoreFx.js';
 import { uploadFile } from '../firebase/firestorageFx.js';
 
 export default () => {
@@ -109,7 +109,9 @@ export default () => {
     const textarea = divElement.querySelector('#post').value;
     const textareaEmpty = divElement.querySelector('#post');
     const inputFile = divElement.querySelector('#file-input').files;
-
+    const inputFileEmpty = divElement.querySelector('#file-input');
+    /* divElement.querySelector('#post').value = ''; */
+    /* divElement.querySelector('#file-input').value = ''; */
     // fx firestorage
     if (textarea.length > 0 || inputFile.length >= 1) {
       if (inputFile.length >= 1) {
@@ -128,8 +130,7 @@ export default () => {
               url,
               [],
             )
-              .then(() => { textareaEmpty.value = ''; })
-              .catch((error) => { console.log('Got an error: ', error); });
+              .then(() => { textareaEmpty.value = ''; inputFileEmpty.value = ''; });
           });
         });
       } else {
@@ -140,11 +141,9 @@ export default () => {
           new Date().toLocaleString(),
           null,
           [])
-          .then(() => { textareaEmpty.value = ''; })
-          .catch((error) => { console.log('Got an error: ', error); });
+          .then(() => { textareaEmpty.value = ''; inputFileEmpty.value = ''; });
       }
     }
-    /*  divElement.querySelector('#post').value = ''; */
   });
 
   return divElement;
